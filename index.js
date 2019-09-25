a4c794a01b61331260705c977f833dd26a2f4824
icydwei <robpaik@gmail.com>
2019-09-25 15:10:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+function map(array, func) {
+  let newArray = []
+  for(let i = 0; i < array.length; i++) {
+    newArray.push(func(array[i]));
+  }
+  return newArray
+}
+
+function reduce(array, func, startingVal) {
+  let memo = (!!startingVal) ? startingVal : array[0]
+  let i = (!!startingVal) ? 0 : 1
+  for (; i < array.length; i++){
+    memo = func(array[i], memo)
+  }
+  return memo;
+}
